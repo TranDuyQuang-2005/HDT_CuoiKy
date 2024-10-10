67d3048c8e21ca30dc2d3fa1bf8b07308ca20c53
TranDuyQuang-2005 <duyquang022005@gmail.com>
2024-10-10 20:17:55 +0700

    Update DeTaiBLL.cs

--- DeTaiBLL.cs
+++ DeTaiBLL.cs
@@ -1,4 +1,4 @@
-﻿using QuanLyDeTaiNghienCuu_DAL;
+using QuanLyDeTaiNghienCuu_DAL;
 using QuanLyDeTaiNghienCuu_DTO;
 using System;
 using System.Collections.Generic;
@@ -8,14 +8,14 @@ namespace QuanLyDeTaiNghienCuu_BLL
     public class DeTaiBLL
     {
         private DeTaiDAL deTaiDAL = new DeTaiDAL();
-        private List<DeTaiNCKHDTO> danhSachDeTai;
+        private List<DeTaiNCKH> danhSachDeTai;
 
         public DeTaiBLL()
         {
             danhSachDeTai = deTaiDAL.DocDanhSachDeTai();
         }
 
-        public void ThemDeTai(DeTaiNCKHDTO deTai)
+        public void ThemDeTai(DeTaiNCKH deTai)
         {
             danhSachDeTai.Add(deTai);
             LuuDanhSachDeTai();
@@ -26,12 +26,12 @@ namespace QuanLyDeTaiNghienCuu_BLL
             deTaiDAL.LuuDanhSachDeTai(danhSachDeTai);
         }
 
-        public List<DeTaiNCKHDTO> DocDanhSachDeTai()
+        public List<DeTaiNCKH> DocDanhSachDeTai()
         {
             return danhSachDeTai;
         }
 
-        public List<DeTaiNCKHDTO> TimKiemDeTaiTheoTen(string keyword)
+        public List<DeTaiNCKH> TimKiemDeTaiTheoTen(string keyword)
         {
             return danhSachDeTai.FindAll(dt =>
                 (dt.TenDeTai != null && dt.TenDeTai.Contains(keyword)) ||  // Kiểm tra null trước khi Contains
@@ -41,7 +41,7 @@ namespace QuanLyDeTaiNghienCuu_BLL
             );
         }
 
-        public List<DeTaiNCKHDTO> TimKiemDeTaiTheoGiangVien(string tenGV)
+        public List<DeTaiNCKH> TimKiemDeTaiTheoGiangVien(string tenGV)
         {
             return danhSachDeTai.FindAll(dt => dt.GiangVienHuongDan.Contains(tenGV));
         }
@@ -61,22 +61,22 @@ namespace QuanLyDeTaiNghienCuu_BLL
         }
 
 
-        public List<DeTaiNCKHDTO> LocDeTaiKinhPhiTren10Trieu()
+        public List<DeTaiNCKH> LocDeTaiKinhPhiTren10Trieu()
         {
             return danhSachDeTai.FindAll(dt => dt.TinhKinhPhi() > 10000000);
         }
 
-        public List<DeTaiNCKHDTO> LocDeTaiLyThuyetApDungThucTe()
+        public List<DeTaiNCKH> LocDeTaiLyThuyetApDungThucTe()
         {
             return danhSachDeTai.FindAll(dt => dt is DeTaiLyThuyet lyThuyet && lyThuyet.ApDungThucTe);
         }
 
-        public List<DeTaiNCKHDTO> LocDeTaiKinhTeSoCauHoiTren100()
+        public List<DeTaiNCKH> LocDeTaiKinhTeSoCauHoiTren100()
         {
             return danhSachDeTai.FindAll(dt => dt is DeTaiKinhTe kinhTe && kinhTe.SoCauHoiKhaoSat > 100);
         }
 
-        public List<DeTaiNCKHDTO> LocDeTaiThoiGianTren4Thang()
+        public List<DeTaiNCKH> LocDeTaiThoiGianTren4Thang()
         {
             return danhSachDeTai.FindAll(dt => (dt.ThoiGianKetThuc - dt.ThoiGianBatDau).TotalDays > 120);
         }
